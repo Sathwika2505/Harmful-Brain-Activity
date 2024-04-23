7605f02ea02a2b984918083514c37a6340af62a1
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-04-23 17:39:47 +0530

    Update trigger_airflow_dag.py

--- trigger_airflow_dag.py
+++ trigger_airflow_dag.py
@@ -11,7 +11,7 @@ print("connecting to instance........")
 ssh_client.connect(hostname = airflow_instance_ip, username='ubuntu', pkey = key)
 print("connected")
 ftp_client=ssh_client.open_sftp()
-commands = ["sudo docker exec 0140c4996948 airflow dags trigger HMS-Brain-Activity"]
+commands = ["sudo docker exec 0140c4996948 airflow dags trigger Harmful-Brain-Activity"]
 
 for cmd in commands:
     stdin, stdout, stderr = ssh_client.exec_command(cmd)
