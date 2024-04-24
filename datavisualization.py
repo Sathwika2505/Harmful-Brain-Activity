7a417c8da33ca7eb334ab4c564729363f389b7d4
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-04-24 11:24:43 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -69,6 +69,7 @@ def load_data_from_s3(bucket_name, file_key):
                       aws_secret_access_key=secret_key,
                       region_name='us-east-1')
         response = s3.get_object(Bucket=bucket_name, Key=file_key)
+        print("------:",response)
         eeg_specs_data = response['Body'].read()
         spectrograms = np.load(io.BytesIO(eeg_specs_data), allow_pickle=True).item()
         print("------------:",spectrograms)
